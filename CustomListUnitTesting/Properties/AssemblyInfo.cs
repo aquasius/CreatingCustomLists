7fb3cc0cccaa895e7559f7413564d8737da15753
Aaron Quasius <aquasius92@gmail.com>
2019-12-05 11:20:00 -0600

    initial commit. Created my first test method that adds one number to the count.

---
+++ CustomListUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1391431e-15a1-4976-8c23-013a8ef59ddc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListUnitTesting/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_OneNumber_ToCount()
+        {
+            //arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //act
+            list1.Add(4);
+            actual = list1.Count;
+
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+    }
+}
