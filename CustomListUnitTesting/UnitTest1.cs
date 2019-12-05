64394c94b95368de50dc070d83feb1ceba66a32b
Aaron Quasius <aquasius92@gmail.com>
2019-12-05 11:49:52 -0600

    created two more test methods for my custom list. Created on that adds five numbers to the list and resizes it. Created another one that fills the list and checks what number is at index three.

--- CustomListUnitTesting/UnitTest1.cs
+++ CustomListUnitTesting/UnitTest1.cs
@@ -25,5 +25,69 @@ namespace CustomListUnitTesting
 
 
         }
+    
+        [TestMethod]
+        public void Add_FiveNumbers_ToResizeCapacity()
+        {
+            //arrange
+            CustomList<int> listResize = new CustomList<int>();
+            int expected = 7;
+            int actual;
+
+            int number1 = 2;
+            int number2 = 4;
+            int number3 = 6;
+            int number4 = 8;
+            int number5 = 10;
+
+
+            //act
+
+            listResize.Add(2);
+            listResize.Add(4);
+            listResize.Add(6);
+            listResize.Add(8);
+            listResize.Add(10);
+
+            actual = listResize.Count;
+
+
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void Check_WhatsAtIndexThree()
+        {
+            //arrange
+            CustomList<int> listCheckIndex = new CustomList<int>();
+            int expected = 4;
+            int actual;
+
+
+            //act
+            listCheckIndex.Add(5);
+            listCheckIndex.Add(2);
+            listCheckIndex.Add(4);
+            listCheckIndex.Add(8);
+
+            actual = listCheckIndex.Count;
+
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+    
+    
+    
+    
+    
     }
 }
